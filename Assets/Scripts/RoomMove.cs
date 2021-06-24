e161b3a29c62e7a60d285ee7c18606e91e03b74d
K-20 <69999193+K-209@users.noreply.github.com>
2021-06-24 10:12:54 -0400

    Add empty project

---
+++ Assets/Scripts/RoomMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomMove : MonoBehaviour
+{
+  private CameraMovement cam;
+  public Vector2 newMaxPos;
+  public Vector2 newMinPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main.GetComponent<CameraMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OntriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == ("Player"))
+      {
+            cam.maxPosition = newMaxPos;
+            cam.minPosition = newMinPos;
+      }
+    }
+}
